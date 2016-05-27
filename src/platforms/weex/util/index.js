2b26e422ab11b7fe3775482fe7ea862a0c594f0f
Jinjiang <zhaojinjiang@me.com>
2016-05-27 15:17:05 +0800

    moved weex/util

--- src/platforms/weex/util/index.js
+++ src/platforms/weex/util/index.js
@@ -1,13 +1,13 @@
 import { makeMap } from 'shared/util'
-import { Node } from './runtime/native'
+import { Node } from '../runtime/native'
 
 export const isReservedTag = makeMap(
   'div,img,image,input,switch,indicator,list,scroller,cell,template,text,slider,image'
 )
-export function isUnaryTag () { console.log('isUnaryTag') }
-export function mustUseProp () { console.log('mustUseProp') }
-export function getTagNamespace () { console.log('getTagNamespace') }
-export function isUnknownElement () { console.log('isUnknownElement') }
+export function isUnaryTag () { /* console.log('isUnaryTag') */ }
+export function mustUseProp () { /* console.log('mustUseProp') */ }
+export function getTagNamespace () { /* console.log('getTagNamespace') */ }
+export function isUnknownElement () { /* console.log('isUnknownElement') */ }
 export function query (el, instanceId) {
   const body = new Node(el)
   body.instanceId = instanceId
