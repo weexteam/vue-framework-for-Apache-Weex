a767f39137387c3ad702095bf6132651a0272054
Jinjiang <zhaojinjiang@me.com>
2016-10-14 09:12:46 +0800

    more test cases for weex class compiler

--- test/weex/compiler/class.spec.js
+++ test/weex/compiler/class.spec.js
@@ -20,4 +20,36 @@ describe('compile class', () => {
     expect(errors[0]).toMatch(/a \{\{b\}\} c/)
     expect(errors[0]).toMatch(/v\-bind/)
   })
+
+  it('should compile class binding of array', () => {
+    const { render, staticRenderFns, errors } = compile(`<div v-bind:class="['a', 'b', c]"></div>`)
+    expect(render).not.toBeUndefined()
+    expect(staticRenderFns).toEqual([])
+    expect(render).toMatch(/class\:\['a', 'b', c\]/)
+    expect(errors).toEqual([])
+  })
+
+  it('should compile class binding of map', () => {
+    const { render, staticRenderFns, errors } = compile(`<div v-bind:class="{ a: true, b: x }"></div>`)
+    expect(render).not.toBeUndefined()
+    expect(staticRenderFns).toEqual([])
+    expect(render).toMatch(/class\:\{ a\: true, b\: x \}/)
+    expect(errors).toEqual([])
+  })
+
+  it('should compile class binding of a variable', () => {
+    const { render, staticRenderFns, errors } = compile(`<div v-bind:class="x"></div>`)
+    expect(render).not.toBeUndefined()
+    expect(staticRenderFns).toEqual([])
+    expect(render).toMatch(/class\:x/)
+    expect(errors).toEqual([])
+  })
+
+  it('should compile class binding by shorthand', () => {
+    const { render, staticRenderFns, errors } = compile(`<div :class="['a', 'b', c]"></div>`)
+    expect(render).not.toBeUndefined()
+    expect(staticRenderFns).toEqual([])
+    expect(render).toMatch(/class\:\['a', 'b', c\]/)
+    expect(errors).toEqual([])
+  })
 })
