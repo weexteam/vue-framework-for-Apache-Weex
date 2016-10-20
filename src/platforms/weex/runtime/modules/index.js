f04a9ed91d1c25ce36187bb84be49a1b1775ad31
Jinjiang <zhaojinjiang@me.com>
2016-10-20 17:07:28 +0800

    Removed dom-props for Weex

--- src/platforms/weex/runtime/modules/index.js
+++ src/platforms/weex/runtime/modules/index.js
@@ -1,13 +1,11 @@
 import attrs from './attrs'
 import klass from './class'
 import events from './events'
-import domProps from './dom-props'
 import style from './style'
 
 export default [
   attrs,
   klass,
   events,
-  domProps,
   style
 ]
