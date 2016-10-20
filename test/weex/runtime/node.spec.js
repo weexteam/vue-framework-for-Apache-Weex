4b3fb2642b973bcd71259e438230cd1dfb683d2f
Jinjiang <zhaojinjiang@me.com>
2016-10-20 21:27:43 +0800

    Added some test cases for vdom diff in Weex.

--- test/weex/runtime/node.spec.js
+++ test/weex/runtime/node.spec.js
@@ -1,4 +1,5 @@
 import {
+  compileAndStringify,
   prepareRuntime,
   resetRuntime,
   createInstance
@@ -63,11 +64,162 @@ describe('node in render function', () => {
   })
 
   it('should be generated with sub components', () => {
-    // todo
+    const instance = createInstance(runtime, `
+      new Vue({
+        render: function (createElement) {
+          return createElement('div', {}, [
+            createElement('text', { attrs: { value: 'Hello' }}, []),
+            createElement('foo', { props: { x: 'Weex' }})
+          ])
+        },
+        components: {
+          foo: {
+            props: {
+              x: { default: 'World' }
+            },
+            render: function (createElement) {
+              return createElement('text', { attrs: { value: this.x }}, [])
+            }
+          }
+        },
+        el: "body"
+      })
+    `)
+    expect(instance.getRealRoot()).toEqual({
+      type: 'div',
+      children: [
+        { type: 'text', attr: { value: 'Hello' }},
+        { type: 'text', attr: { value: 'Weex' }}
+      ]
+    })
   })
 
-  it('should be generated with module diff', () => {
-    // todo
+  it('should be generated with module diff', (done) => {
+    const instance = createInstance(runtime, `
+      new Vue({
+        data: {
+          counter: 0
+        },
+        render: function (createElement) {
+          switch (this.counter) {
+            case 1:
+            return createElement('div', {}, [
+              createElement('text', { attrs: { value: 'Hello' }}, []),
+              createElement('text', { attrs: { value: 'World' }}, [])
+            ])
+
+            case 2:
+            return createElement('div', {}, [
+              createElement('text', { attrs: { value: 'Hello' }}, []),
+              createElement('text', { attrs: { value: 'World' }}, []),
+              createElement('text', { attrs: { value: 'Weex' }}, [])
+            ])
+
+            case 3:
+            return createElement('div', {}, [
+              createElement('text', { attrs: { value: 'Hello' }}, []),
+              createElement('text', { attrs: { value: 'Weex' }}, [])
+            ])
+
+            case 4:
+            return createElement('div', {}, [
+              createElement('text', { attrs: { value: 'Weex' }}, [])
+            ])
+
+            case 5:
+            return createElement('div', {}, [
+              createElement('text', { attrs: { value: 'Hello' }}, []),
+              createElement('text', { attrs: { value: 'Weex' }}, [])
+            ])
+
+            case 6:
+            return createElement('div', {}, [
+              createElement('input', { attrs: { value: 'Hello' }}, []),
+              createElement('text', { attrs: { value: 'Weex' }}, [])
+            ])
+
+            default:
+            return createElement('div', {}, [
+              createElement('text', { attrs: { value: 'Hello' }}, []),
+            ])
+          }
+        },
+        el: "body"
+      })
+    `)
+    expect(instance.getRealRoot()).toEqual({
+      type: 'div',
+      children: [
+        { type: 'text', attr: { value: 'Hello' }}
+      ]
+    })
+
+    instance.$refresh({ counter: 1 })
+    setTimeout(() => {
+      expect(instance.getRealRoot()).toEqual({
+        type: 'div',
+        children: [
+          { type: 'text', attr: { value: 'Hello' }},
+          { type: 'text', attr: { value: 'World' }}
+        ]
+      })
+
+      instance.$refresh({ counter: 2 })
+      setTimeout(() => {
+        expect(instance.getRealRoot()).toEqual({
+          type: 'div',
+          children: [
+            { type: 'text', attr: { value: 'Hello' }},
+            { type: 'text', attr: { value: 'World' }},
+            { type: 'text', attr: { value: 'Weex' }}
+          ]
+        })
+
+        instance.$refresh({ counter: 3 })
+        setTimeout(() => {
+          expect(instance.getRealRoot()).toEqual({
+            type: 'div',
+            children: [
+              { type: 'text', attr: { value: 'Hello' }},
+              { type: 'text', attr: { value: 'Weex' }}
+            ]
+          })
+
+          instance.$refresh({ counter: 4 })
+          setTimeout(() => {
+            expect(instance.getRealRoot()).toEqual({
+              type: 'div',
+              children: [
+                { type: 'text', attr: { value: 'Weex' }}
+              ]
+            })
+
+            instance.$refresh({ counter: 5 })
+            setTimeout(() => {
+              expect(instance.getRealRoot()).toEqual({
+                type: 'div',
+                children: [
+                  { type: 'text', attr: { value: 'Hello' }},
+                  { type: 'text', attr: { value: 'Weex' }}
+                ]
+              })
+
+              instance.$refresh({ counter: 6 })
+              setTimeout(() => {
+                expect(instance.getRealRoot()).toEqual({
+                  type: 'div',
+                  children: [
+                    { type: 'input', attr: { value: 'Hello' }},
+                    { type: 'text', attr: { value: 'Weex' }}
+                  ]
+                })
+                done()
+              })
+            })
+          })
+        })
+      })
+    })
   })
 
   it('should be generated with if/for diff', () => {
