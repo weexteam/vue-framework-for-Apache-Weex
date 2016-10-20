a535e1b670f70d259f3a9f31a8b8fcb56960c3fb
Jinjiang <zhaojinjiang@me.com>
2016-10-21 02:07:49 +0800

    More comments for Weex framework code.

--- src/entries/weex-framework.js
+++ src/entries/weex-framework.js
@@ -10,6 +10,10 @@ const {
 let activeId = undefined
 const oriIsReservedTag = (Vue && Vue.config && typeof Vue.config.isReservedTag === 'function') ? Vue.config.isReservedTag : function () {}
 
+/**
+ * Prepare framework config, basically about the virtual-DOM and JS bridge.
+ * @param {object} cfg
+ */
 export function init (cfg) {
   renderer.Document = cfg.Document
   renderer.Element = cfg.Element
@@ -17,6 +21,9 @@ export function init (cfg) {
   renderer.sendTasks = cfg.sendTasks
 }
 
+/**
+ * Reset framework config and clear all registrations.
+ */
 export function reset () {
   clear(instances)
   clear(modules)
@@ -28,22 +35,43 @@ export function reset () {
   Vue.config.isReservedTag = oriIsReservedTag
 }
 
+/**
+ * Delete all keys of an object.
+ * @param {object} obj
+ */
 function clear (obj) {
   for (const key in obj) {
     delete obj[key]
   }
 }
 
+/**
+ * Create an instance with id, code, config and external data.
+ * @param {string} instanceId
+ * @param {string} appCode
+ * @param {object} config
+ * @param {object} data
+ */
 export function createInstance (
   instanceId, appCode, config /* {bundleUrl, debug} */, data) {
+  // Set active instance id and put some information into `instances` map.
   activeId = instanceId
   instances[instanceId] = {
     instanceId, config, data,
+    // Virtual-DOM object.
     document: new renderer.Document(instanceId, config.bundleUrl),
+    // All function/callback of parameters before sent to native
+    // will be converted as an id. So `callbacks` is used to store
+    // these real functions. When a callback invoked and won't be
+    // called again, it should be removed from here automatically.
     callbacks: [],
+    // The latest callback id, incremental.
     callbackId: 1
   }
 
+  // The function which create a closure the JS Bundle will run in.
+  // It will declare some global variables like `Vue`, HTML5 Timer APIs,
+  // and native module getter.
   const start = new Function(
     'Vue',
     '__weex_require_module__',
@@ -52,13 +80,19 @@ export function createInstance (
     'clearTimeout',
     'clearInterval',
     appCode)
+
+  // Each instance has a independent `Vue` object and it should have
+  // all top-level public APIs.
   const subVue = Vue.extend({});
   ['util', 'set', 'delete', 'nextTick', 'use'].forEach(name => {
     subVue[name] = Vue[name]
   })
 
+  // Prepare native module getter and HTML5 Timer APIs.
   const moduleGetter = genModuleGetter(instanceId)
   const timerAPIs = getInstanceTimer(instanceId, moduleGetter)
+
+  // Run the JS Bundle and send `createFinish` signal to native.
   start(
     subVue, moduleGetter,
     timerAPIs.setTimeout,
@@ -68,6 +102,11 @@ export function createInstance (
   renderer.sendTasks(instanceId + '', [{ module: 'dom', method: 'createFinish', args: [] }], -1)
 }
 
+/**
+ * Destroy an instance with id. It will make sure all memory of
+ * this instance released and no more leaks.
+ * @param {string} instanceId
+ */
 export function destroyInstance (instanceId) {
   const instance = instances[instanceId] || {}
   if (instance.app instanceof Vue) {
@@ -76,6 +115,13 @@ export function destroyInstance (instanceId) {
   delete instances[instanceId]
 }
 
+/**
+ * Refresh an instance with id and new top-level component data.
+ * It will use `Vue.set` on all keys of the new data. So it's better
+ * define all possible meaningful keys when instance created.
+ * @param {string} instanceId
+ * @param {object} data
+ */
 export function refreshInstance (instanceId, data) {
   const instance = instances[instanceId] || {}
   if (!instance.app instanceof Vue) {
@@ -84,9 +130,14 @@ export function refreshInstance (instanceId, data) {
   for (const key in data) {
     Vue.set(instance.app, key, data[key])
   }
+  // Finally `refreshFinish` signal needed.
   renderer.sendTasks(instanceId + '', [{ module: 'dom', method: 'refreshFinish', args: [] }], -1)
 }
 
+/**
+ * Get the JSON object of the root element.
+ * @param {string} instanceId
+ */
 export function getRoot (instanceId) {
   const instance = instances[instanceId] || {}
   if (!instance.app instanceof Vue) {
@@ -95,6 +146,13 @@ export function getRoot (instanceId) {
   return instance.app.$el.toJSON()
 }
 
+/**
+ * Receive tasks from native. Generally there are two types of tasks:
+ * 1. `fireEvent`: an device actions or user actions from native.
+ * 2. `callback`: invoke function which sent to native as a parameter before.
+ * @param {string} instanceId
+ * @param {array}  tasks
+ */
 export function receiveTasks (instanceId, tasks) {
   const instance = instances[instanceId] || {}
   if (!instance.app instanceof Vue) {
@@ -102,26 +160,33 @@ export function receiveTasks (instanceId, tasks) {
   }
   const { callbacks, document } = instance
   tasks.forEach(task => {
+    // `fireEvent` case: find the event target and fire.
     if (task.method === 'fireEvent') {
       const [nodeId, type, e, domChanges] = task.args
       const el = document.getRef(nodeId)
       document.fireEvent(el, type, e, domChanges)
     }
+    // `callback` case: find the callback by id and call it.
     if (task.method === 'callback') {
       const [callbackId, data, ifKeepAlive] = task.args
       const callback = callbacks[callbackId]
       if (typeof callback === 'function') {
         callback(data)
+        // Remove the callback from `callbacks` if it won't called again.
         if (typeof ifKeepAlive === 'undefined' || ifKeepAlive === false) {
           callbacks[callbackId] = undefined
         }
       }
     }
   })
-
+  // Finally `updateFinish` signal needed.
   renderer.sendTasks(instanceId + '', [{ module: 'dom', method: 'updateFinish', args: [] }], -1)
 }
 
+/**
+ * Register native modules information.
+ * @param {object} newModules
+ */
 export function registerModules (newModules) {
   for (const name in newModules) {
     if (!modules[name]) {
@@ -138,6 +203,10 @@ export function registerModules (newModules) {
   }
 }
 
+/**
+ * Register native components information.
+ * @param {array} newComponents
+ */
 export function registerComponents (newComponents) {
   const config = Vue.config
   const newConfig = {}
@@ -161,12 +230,14 @@ export function registerComponents (newComponents) {
   }
 }
 
+// Hack `Vue` behavior to handle instance information and data
+// before root component created.
 Vue.mixin({
   beforeCreate () {
     const options = this.$options
     const parentOptions = (options.parent && options.parent.$options) || {}
 
-    // root vm
+    // root component (vm)
     if (options.el) {
 
       // record instance info
@@ -191,6 +262,10 @@ Vue.mixin({
   }
 })
 
+/**
+ * Get instance config.
+ * @return {object}
+ */
 Vue.prototype.$getConfig = function () {
   const instance = instances[this.$instanceId] || {}
   if (instance.app instanceof Vue) {
@@ -198,6 +273,16 @@ Vue.prototype.$getConfig = function () {
   }
 }
 
+/**
+ * Generate native module getter. Each native module has several
+ * methods to call. And all the hebaviors is instance-related. So
+ * this getter will return a set of methods which additionally
+ * send current instance id to native when called. Also the args
+ * will be normalized into "safe" value. For example function arg
+ * will be converted into a callback id.
+ * @param  {string}  instanceId
+ * @return {function}
+ */
 function genModuleGetter (instanceId) {
   const instance = instances[instanceId]
   return function (name) {
@@ -216,6 +301,15 @@ function genModuleGetter (instanceId) {
   }
 }
 
+/**
+ * Generate HTML5 Timer APIs. An important point is that the callback
+ * will be converted into callback id when sent to native. So the
+ * framework can make sure no side effect of the callabck happened after
+ * an instance destroyed.
+ * @param  {[type]} instanceId   [description]
+ * @param  {[type]} moduleGetter [description]
+ * @return {[type]}              [description]
+ */
 function getInstanceTimer (instanceId, moduleGetter) {
   const instance = instances[instanceId]
   const timer = moduleGetter('timer')
@@ -244,6 +338,16 @@ function getInstanceTimer (instanceId, moduleGetter) {
   return timerAPIs
 }
 
+/**
+ * Convert all type of values into "safe" format to send to native.
+ * 1. A `function` will be converted into callback id.
+ * 2. An `Element` object will be converted into `ref`.
+ * The `instance` param is used to generate callback id and store
+ * function if necessary.
+ * @param  {any}    v
+ * @param  {object} instance
+ * @return {any}
+ */
 function normalize (v, instance) {
   const type = typof(v)
 
@@ -272,6 +376,12 @@ function normalize (v, instance) {
   }
 }
 
+/**
+ * Get the exact type of an object by `toString()`. For example call
+ * `toString()` on an array will be returned `[object Array]`.
+ * @param  {any}    v
+ * @return {string}
+ */
 function typof (v) {
   const s = Object.prototype.toString.call(v)
   return s.substring(8, s.length - 1).toLowerCase()
